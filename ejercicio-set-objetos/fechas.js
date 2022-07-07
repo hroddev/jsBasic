c5ffd06d7b5e413a163c0adaae06da84cfd31211
Hector Rodriguez <hector@hrod.dev>
2022-07-07 08:25:20 -0500

    add files of date test

---
+++ ejercicio-set-objetos/fechas.js
@@ -0,0 +1,27 @@
+/* Enunciado del ejercicio:
+
+Crea un archivo llamado fechas.js que contenga las siguientes líneas
+
+- La fecha de hoy
+
+- La fecha de tu nacimiento
+
+- Un variable que indique si hoy es más tarde (>) que la fecha de tu nacimiento
+
+- Una variable que contenga el día de tu nacimiento
+
+- Una variable que contenga el mes de tu nacimiento (recuerda que Enero es mes 0)
+
+- Una variable que contenga el año de tu nacimiento (con 4 dígitos) */
+
+let fechaHoy = new Date()
+console.log(fechaHoy)
+
+let fechaNacimiento = new Date('Oct 13 1980')
+console.log(fechaNacimiento)
+
+let mesNacimiento = fechaNacimiento.getMonth() + 1
+console.log(mesNacimiento)
+
+let anyoNacimiento = fechaNacimiento.getFullYear()
+console.log(anyoNacimiento)
